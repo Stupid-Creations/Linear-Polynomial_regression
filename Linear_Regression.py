c31c0a6c2b0219c7fb6e9412ca86f286e4931672
Stupid-Creations <97241429+Stupid-Creations@users.noreply.github.com>
2023-06-15 12:35:11 +0530

    Add cost function
    Now displays error

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -9,9 +9,12 @@ class LinearRegression:
             x[i].insert(0,1)
         return x
     def predict(self,X):
-        return dot(self.change_input(v_m(X)),self.params)
+        return m_v(dot(self.change_input(v_m(X)),self.params))
     def gradient(self,X,Y):
         x,y = self.change_input(v_m(X)),v_m(Y)
         return dot(getMatrixInverse(dot(transpose(x),x)),dot(transpose(x),y))
     def fit(self,X,Y):
         self.params = self.gradient(X,Y)
+        print("The error of the model is: ",self.cost(X,Y))
+    def cost(self,X,Y):
+        return sum([(a-b)**2 for a,b in zip(self.predict(X),Y)])/len(Y)
