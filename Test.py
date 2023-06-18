dc2a7bd41a08a840e7b6fbe928c925bdaf74ef88
Stupid-Creations <97241429+Stupid-Creations@users.noreply.github.com>
2023-06-18 13:20:13 +0530

    Change to fit with changes in LinearRegression.py

--- Test.py
+++ Test.py
@@ -1,15 +1,15 @@
 from matplotlib import pyplot as plt
 from Linear_Regression import *
 
-#test dataset of celsius to farenheit
+#test dataset of celsius to kelvin
 x = [i for i in range(50)]
 y = [a+273 for a in x]
 
 testx = [i for i in range(51,100)]
 testy = [i+273 for i in testx]
 
-model = LinearRegression(x,y)
-yn = m_v(model.predict(testx))
+model = LinearRegression(v_m(x),v_m(y))
+yn = model.predict(v_m(testx))
 
 print("Parameters: ",model.params,"There is small deviation in the intercept due to rounding errors")
 
