44cd63b416c9bd1936d45bbce5f3f0c90d9d2c3e
Stupid-Creations <97241429+Stupid-Creations@users.noreply.github.com>
2023-06-18 13:18:31 +0530

    Update Linear_Regression.py

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -1,20 +1,20 @@
 from Matrix import *
+#NOTE: YOU NEED TO USE v_m(data) IF YOU ARE USING DATA IN FORM OF 1D ARRAY
 class LinearRegression:
     def __init__(self,X,Y):
         self.fit(X,Y)
     def change_input(self,X):
-        x = X
+        x = copy_matrix(X)
         for i in range(len(x)):
             x[i].insert(0,1)
         return x
     def predict(self,X):
-        return m_v(dot(self.change_input(v_m(X)),self.params))
-    def gradient(self,X,Y,oneindependent=True):
-        if oneindependent:
-            x,y = self.change_input(v_m(X)),v_m(Y)
-        return dot(getMatrixInverse(dot(transpose(x),x)),dot(transpose(x),y))
+        return m_v(dot(self.change_input(X),self.params))
+    def gradient(self,X,Y):
+        x = self.change_input(X)
+        return dot(getMatrixInverse(dot(transpose(x),x)),dot(transpose(x),Y))
     def fit(self,X,Y):
         self.params = self.gradient(X,Y)
         print("The error of the model is: ",self.cost(X,Y))
     def cost(self,X,Y):
-        return sum([(a-b)**2 for a,b in zip(self.predict(X),Y)])/len(Y)
+        return sum([(a-b)**2 for a,b in zip(self.predict(X),m_v(Y))])/len(Y)
