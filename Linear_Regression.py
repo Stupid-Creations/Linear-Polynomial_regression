2c34695571e8c27bee571f5c8663e485ed2c8a62
Raghav <97241429+Stupid-Creations@users.noreply.github.com>
2023-06-19 16:24:33 +0530

    Added polynomial regression

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -1,23 +1,33 @@
 from Matrix import *
-#NOTE: PLEASE USE v_m(data) IF ANY OF YOUR DATA IS A 1D ARRAY
+# use v_m for 1D arrays
 class LinearRegression:
-    def __init__(self,X,Y):
-        self.fit(X,Y)
-    def change_input(self,X):
-        x = copy_matrix(X)
+    def __init__(self,X,Y,poly1=False,degree=1):
+        self.fit(X,Y,poly1,degree)
+    def change_input(self,X,poly=False,degree = 1):
+        if poly==False:
+            x = copy_matrix(X)
+            for i in range(len(x)):
+                x[i].insert(0,1)
+            return x
+        elif poly == True:
+            a = self.change_ip(X,degree)
+            return a
+    def change_ip(self,X,degree):
+        x = self.change_input(copy_matrix(X))
         for i in range(len(x)):
-            x[i].insert(0,1)
+            for j in range(len(x[0])):
+                x[i][j] = x[i][j]**(j+(degree-1))
         return x
-    def predict(self,X):
-        return m_v(dot(self.change_input(X),self.params))
-    def gradient(self,X,Y):
-        x = self.change_input(X)
+    def predict(self,X,poly=False,degree=1):
+        return m_v(dot(self.change_input(X,poly,degree),self.params))
+    def gradient(self,X,Y,poly=False,degree=1):
+        x = self.change_input(X,poly,degree)
         try:
             return dot(getMatrixInverse(dot(transpose(x),x)),dot(transpose(x),Y))
         except:
             raise ValueError("Input values dependent")
-    def fit(self,X,Y):
-        self.params = self.gradient(X,Y)
-        print("The error of the model is: ",self.cost(X,Y))
-    def cost(self,X,Y):
-        return sum([(a-b)**2 for a,b in zip(self.predict(X),m_v(Y))])/len(Y)
+    def fit(self,X,Y,poly=False,degree=1):
+        self.params = self.gradient(X,Y,poly,degree)
+        print("The error of the model is: ",self.cost(X,Y,poly,degree))
+    def cost(self,X,Y,poly=False,degree=1):
+        return sum([(a-b)**2 for a,b in zip(self.predict(X,poly,degree),m_v(Y))])/len(Y)
